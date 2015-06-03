cbaaf7ed67d83bb39cf90bbfb651b165a8ebce16
chandu0101 <chandu.csu2010@gmail.com>
2015-06-03 15:18:52 +0530

    scalajs-react 0.9 and scalacss

--- src/main/scala/com/chandu0101/scalajs/react/template/components/Footer.scala
+++ src/main/scala/com/chandu0101/scalajs/react/template/components/Footer.scala
@@ -6,7 +6,7 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 object Footer {
 
   val component = ReactComponentB.static("Footer",
-    <.footer(^.paddingTop := "30px", ^.textAlign.center,
+    <.footer(^.textAlign.center,
     <.div(^.borderBottom := "1px solid grey", ^.padding := "0px"),
     <.p(^.paddingTop := "5px", "Built using scalajs/scalajs-react/scalacss")
     )
